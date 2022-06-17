014dbb7fc1b24712f2f34ee9110e31340a40a0f5
sabrinariyani <sabrinaryn20@gmail.com>
2022-06-17 20:07:48 +0700

    tes

--- src/components/newarrivals/Ndata.js
+++ src/components/newarrivals/Ndata.js
@@ -6,28 +6,28 @@ const Ndata = [
   },
   {
     cover: "./images/arrivals/arrivals2.png",
-    name: "Makeup",
-    price: "250",
+    name: "",
+    price: "",
   },
   {
     cover: "./images/arrivals/arrivals3.png",
-    name: "Smart Watch",
-    price: "50",
+    name: "",
+    price: "",
   },
   {
     cover: "./images/arrivals/arrivals4.png",
-    name: "Lipstick",
-    price: "15",
+    name: "",
+    price: "",
   },
   {
     cover: "./images/arrivals/arrivals5.png",
-    name: "Green Plant",
-    price: "10",
+    name: "",
+    price: "",
   },
   {
     cover: "./images/arrivals/arrivals6.png",
-    name: "Bonsai tree",
-    price: "400",
+    name: "",
+    price: "",
   },
 ]
 
